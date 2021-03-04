a6bf87f7c0f77da0d53fabcc93a9f0b8bceeecfa
Hussein512 <74354782+Hussein512@users.noreply.github.com>
2021-03-04 23:39:02 +0200

    Revert "my code"

--- android/app/src/main/kotlin/hashbro/com/mywhatsapp/MainActivity.kt
+++ android/app/src/main/kotlin/hashbro/com/mywhatsapp/MainActivity.kt
@@ -1,10 +1,6 @@
 package hashbro.com.mywhatsapp
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-
-
-
 }
--- lib/model/users.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class users {
-  final String name;
-  final String avatarUrl;
-
-  users({this.name, this.avatarUrl});
-}
